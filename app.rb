1ebee1f70d4a60f616540bea1d6fe6f5e87c36bd
Matthew Rostkowski <mcr2225@columbia.edu>
2025-09-22 04:22:13 +0000

    Change Hello World to Goodbye World

--- app.rb
+++ app.rb
@@ -4,6 +4,6 @@ class MyApp < Sinatra::Base
   set :host_authorization, { permitted_hosts: [] }
 
   get '/' do
-    '<!DOCTYPE html><html><body><h1>Hello World</h1></body></html>'
+    '<!DOCTYPE html><html><body><h1>Goodbye World</h1></body></html>'
   end
 end
